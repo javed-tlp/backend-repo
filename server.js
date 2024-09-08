7f14022112c663c439495b223cbe1e3158ffc9ec
javed-tlp <saifijaved616@gmail.com>
2024-09-08 22:44:52 +0530

    "Moved dotenv config, added path module, updated CORS origin, served static files, and changed default route to serve index.html"

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-require('dotenv').config(); // Load environment variables
-
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const contactRoutes = require('./routes/contactRoutes');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://main--strong-raindrop-c0e6b5.netlify.app/' // Replace with your actual frontend URL
+    origin: 'https://main--strong-raindrop-c0e6b5.netlify.app/'  // Replace with your actual frontend URL
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Define a default route
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Define a default route for the root URL
 app.get('/', (req, res) => {
-    res.send('Welcome to the API!');
+    res.sendFile(path.join(__dirname, 'index.html')); // Serve index.html from the root
 });
 
 // Serve API routes
@@ -26,5 +29,4 @@ app.use('/send-email', contactRoutes);
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log('EMAIL_USER:', process.env.EMAIL_USER); // Check if EMAIL_USER is loaded
 });
